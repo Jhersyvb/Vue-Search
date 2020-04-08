d952a5717badd717f9b783e003b0dfe4527a83bb
Jhersy <jhersyvb@gmail.com>
2020-04-08 12:32:12 -0500

    Construyendo store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,32 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import movies from "./movies";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import movies from './movies'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     movies: movies,
     filter: {
-      query: ""
+      query: '',
+      available: true
     }
   },
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  mutations: {
+    SET_QUERY(state, query) {
+      state.filter.query = query
+    },
+    SER_AVAILABLE(state) {
+      state.filter.available = !state.filter.available
+    }
+  },
+  getters: {
+    filteredMovies(state) {
+      let movies = state.movies.filter(movie => movie.available === state.filter.available)
+      if (state.filter.query.length > 2) {
+        return movies.filter(movie => movie.title.toLowerCase().includes(state.filter.query.toLowerCase()))
+      }
+      return movies
+    }
+  }
+})
